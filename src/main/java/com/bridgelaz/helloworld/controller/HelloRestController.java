fa9a1613936c7504740a6d63841a176b59990218
shindeanant <shindeanant1997@gmail.com>
2021-12-12 20:18:13 +0530

    Added: Spring App To display message Using REST controller.

---
+++ src/main/java/com/bridgelaz/helloworld/controller/HelloRestController.java
@@ -0,0 +1,14 @@
+package com.bridgelaz.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    
+    @RequestMapping("/")
+    public String index() {
+        return "Hello From BridgeLabz Using Rest Controller";
+        
+    }
+}
